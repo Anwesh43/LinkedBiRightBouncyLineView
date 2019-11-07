359c8ccb74eb167f4c8b6f67c7f57db55dc1390d
Anwesh43 <anweshthecool0@gmail.com>
2019-11-07 11:04:13 +0530

    adding BiRightBouncyLineView class

--- birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
+++ birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
@@ -50,3 +50,21 @@ fun Canvas.drawBRBLNode(i : Int, scale : Float, paint : Paint) {
     drawBiRightBouncyLine(size, scale, paint)
     restore()
 }
+
+class BiRightBouncyLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
