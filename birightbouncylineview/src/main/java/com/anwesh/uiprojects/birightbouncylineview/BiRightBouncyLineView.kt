8d8a3b18556d776c8ac7ef41ffd53ecb963a5ff9
Anwesh43 <anweshthecool0@gmail.com>
2019-11-07 11:33:09 +0530

    adding BiRightBouncyLine class

--- birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
+++ birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
@@ -134,7 +134,6 @@ class BiRightBouncyLineView(ctx : Context) : View(ctx) {
 
         fun draw(canvas : Canvas, paint : Paint) {
             canvas.drawBRBLNode(i, state.scale, paint)
-            next?.draw(canvas, paint)
         }
 
         fun update(cb : (Float) -> Unit) {
@@ -155,6 +154,29 @@ class BiRightBouncyLineView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class BiRightBouncyLine(var i : Int) {
+
+        private var curr : BRBLNode = BRBLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
