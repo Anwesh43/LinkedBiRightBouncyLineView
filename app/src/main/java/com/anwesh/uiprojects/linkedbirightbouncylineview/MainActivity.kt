8a62206ced8dd4896629f926c443169b420e114b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-07 11:50:22 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbirightbouncylineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbirightbouncylineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbirightbouncylineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.birightbouncylineview.BiRightBouncyLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiRightBouncyLineView.create(this)
     }
 }
