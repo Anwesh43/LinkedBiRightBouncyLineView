8613faf37feb6b05901f36a401c08b0493b7aba5
Anwesh43 <anweshthecool0@gmail.com>
2019-11-07 11:01:47 +0530

    adding drawBRBLNode canvas extension method

--- birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
+++ birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
@@ -14,14 +14,39 @@ import android.view.MotionEvent
 
 val nodes : Int = 5
 val lines : Int = 2
-val scGap : Float = 0.02f
+val scGap : Float = 0.01f
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiRightBouncyLine(size : Float, scale : Float, paint : Paint) {
+    for (j in 0..1) {
+        val sf : Float = scale.divideScale(0, 2).sinify()
+        save()
+        rotate(90f * j)
+        drawLine(0f, 0f, 0f, -sf * size, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawBRBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBiRightBouncyLine(size, scale, paint)
+    restore()
+}
