e6186214ab1904f61cbf5169ccb004949ec6ee91
Anwesh43 <anweshthecool0@gmail.com>
2019-11-07 10:53:26 +0530

    adding constants and libraries

--- birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
+++ birightbouncylineview/src/main/java/com/anwesh/uiprojects/birightbouncylineview/BiRightBouncyLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.birightbouncylineview
 /**
  * Created by anweshmishra on 07/11/19.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
